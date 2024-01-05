e6b43e90014b148b0f811faab655d4c7c40f593c
Axemasta <33064621+Axemasta@users.noreply.github.com>
2024-01-05 16:53:16 +0000

    Build props now toggles proxy
    There are csproj flips and compile constants so no code needs commenting out

--- src/SwapiApp/MauiProgram.cs
+++ src/SwapiApp/MauiProgram.cs
@@ -29,22 +29,25 @@ public static class MauiProgram
             .ConfigureHttpClient(client =>
             {
                 client.BaseAddress = new Uri("https://swapi.dev/api/");
-            });
-//             .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler()
-//             {
-//                 UseProxy = true,
-//                 Proxy = new WebProxy()
-//                 {
-// #if IOS
-//                     Address = new Uri("http://127.0.0.1:8888"),                 
-// #endif
-// #if ANDROID
-//                     // Android currently not working, required to run these steps
-//                     // https://gist.github.com/twaddington/54eda4951fd8d2e858b537bef5f22334
-//                     Address = new Uri("http://10.0.0.2:8888"),         
-// #endif
-//                 }
-//             });
+            })
+#if USE_PROXY
+        .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler()
+            {
+                UseProxy = true,
+                Proxy = new WebProxy()
+                {
+#if IOS
+                    Address = new Uri("http://127.0.0.1:8888"),                 
+#endif
+#if ANDROID
+                    // Android currently not working, required to run these steps
+                    // https://gist.github.com/twaddington/54eda4951fd8d2e858b537bef5f22334
+                    Address = new Uri("http://10.0.0.2:8888"),         
+#endif
+                }
+            })
+#endif
+            ;
 
         return builder.Build();
     }
