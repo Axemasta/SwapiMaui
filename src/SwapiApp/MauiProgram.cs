cb8316da4eee6cda4332fea49732e260103e7463
Axemasta <33064621+Axemasta@users.noreply.github.com>
2024-01-08 13:47:32 +0000

    Updated Sample To Work On Android
    I have got everything working on android now, the manual handler was not needed so this is good

--- src/SwapiApp/MauiProgram.cs
+++ src/SwapiApp/MauiProgram.cs
@@ -30,20 +30,13 @@ public static class MauiProgram
             {
                 client.BaseAddress = new Uri("https://swapi.dev/api/");
             })
-#if USE_PROXY
+#if USE_PROXY && IOS
         .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler()
             {
                 UseProxy = true,
                 Proxy = new WebProxy()
                 {
-#if IOS
                     Address = new Uri("http://127.0.0.1:8888"),                 
-#endif
-#if ANDROID
-                    // Android currently not working, required to run these steps
-                    // https://gist.github.com/twaddington/54eda4951fd8d2e858b537bef5f22334
-                    Address = new Uri("http://10.0.0.2:8888"),         
-#endif
                 }
             })
 #endif
